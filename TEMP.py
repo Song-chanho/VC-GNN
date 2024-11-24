c45b9bfa6a5c73ad382874d5432dd07f3561272a
Chanho Song <sch0622@postech.ac.kr>
2024-11-24 23:37:00 +0900

    Fix : dataset.py to generate data for Vertex cover

--- TEMP.py
+++ TEMP.py
@@ -1,47 +1,17 @@
+import sys, os, argparse, time, datetime
 import numpy as np
-from pulp import LpProblem, LpVariable, lpSum, LpMinimize
+import random
+import networkx as nx
+from concorde.tsp import TSPSolver
+from redirector import Redirector
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum
 
-def ilp_vertex_cover(Ma):
-    """
-    Solves the Vertex Cover problem exactly using Integer Linear Programming (ILP).
-    Args:
-        Ma (numpy.ndarray): Adjacency matrix of the graph.
+n=10
+Ma = np.zeros((n, n))
+for i in range(n):
+    Ma[i,i] = 0
+    for j in range(i+1,n):
+        Ma[i,j] = Ma[j,i] = int(np.random.rand() < 0.5)
 
-    Returns:
-        list: List of vertices in the Vertex Cover.
-    """
-    n = Ma.shape[0]
-    
-    problem = LpProblem("Vertex_Cover", LpMinimize)
-
-    # decision variables. ex) if x_i = 1, vertex i is in the vertex cover.
-    x = {i: LpVariable(f"x_{i}", cat="Binary") for i in range(n)}
-
-    # objective function
-    problem += lpSum([x[i] for i in range(n)])
-
-    # constraint: each edge is covered by at least one vertex
-    for i in range(n):
-        for j in range(i + 1, n):
-            if Ma[i, j] == 1:
-                problem += x[i] + x[j] >= 1
-
-    problem.solve()
-
-    return [i for i in range(n) if x[i].varValue == 1]
-
-# Test graph (Adjacency Matrix)
-# Graph:
-# 0 -- 1
-# |    
-# 2 -- 3
-Ma = np.array([
-    [0, 1, 1, 0],
-    [1, 0, 0, 0],
-    [1, 0, 0, 1],
-    [0, 0, 1, 0]
-])
-
-# Run the function and print the result
-vertex_cover = ilp_vertex_cover(Ma)
-print("Vertex Cover:", vertex_cover)
+for (i,j) in zip(list(np.nonzero(Ma))[0], list(np.nonzero(Ma))[1]):
+    print(i,j)
--- dataset.py
+++ dataset.py
@@ -84,7 +84,7 @@ def create_graph(n, connectivity, distances='euc_2D', metric=True):
 
     # Init adjacency and weight matrices
     Ma = np.zeros((n,n))
-    Mw = np.zeros((n,n))
+    #Mw = np.zeros((n,n))
 
     # Define adjacencies
     for i in range(n):
@@ -95,55 +95,56 @@ def create_graph(n, connectivity, distances='euc_2D', metric=True):
     #end
 
     # Define weights
-    nodes = None
-    if distances == 'euc_2D':
-        # Select 'n' points in the √2/2 × √2/2 square uniformly at random
-        nodes = np.random.rand(n,2)
-        for i in range(n):
-            for j in range(i+1,n):
-                Mw[i,j] = Mw[j,i] = np.sqrt(sum((nodes[i,:]-nodes[j,:])**2))
-            #end
-        #end
-    elif distances == 'random':
-        # Init all weights uniformly at random
-        for i in range(n):
-            for j in range(i+1,n):
-                Mw[j,i] = Mw[i,j] = np.random.rand()
-            #end
-        #end
-    #end
+    # nodes = None
+    # if distances == 'euc_2D':
+    #     # Select 'n' points in the √2/2 × √2/2 square uniformly at random
+    #     nodes = np.random.rand(n,2)
+    #     for i in range(n):
+    #         for j in range(i+1,n):
+    #             Mw[i,j] = Mw[j,i] = np.sqrt(sum((nodes[i,:]-nodes[j,:])**2))
+    #         #end
+    #     #end
+    # elif distances == 'random':
+    #     # Init all weights uniformly at random
+    #     for i in range(n):
+    #         for j in range(i+1,n):
+    #             Mw[j,i] = Mw[i,j] = np.random.rand()
+    #         #end
+    #     #end
+    # #end
 
-    # Enforce metric property, if requested
-    if metric and distances != 'euc_2D':
-        # Create networkx graph G
-        G = nx.Graph()
-        G.add_nodes_from(range(n))
-        G.add_edges_from([ (i,j,{'weight':Mw[i,j]}) for i in range(n) for j in range(n) ])
+    # # Enforce metric property, if requested
+    # if metric and distances != 'euc_2D':
+    #     # Create networkx graph G
+    #     G = nx.Graph()
+    #     G.add_nodes_from(range(n))
+    #     G.add_edges_from([ (i,j,{'weight':Mw[i,j]}) for i in range(n) for j in range(n) ])
 
-        for i in range(n):
-            for j in range(n):
-                if i != j:
-                    Mw[i,j] = nx.shortest_path_length(G,source=i,target=j,weight='weight')
-                else:
-                    Mw[i,j] = 0
-                #end
-            #end
-        #end
-    #end
+    #     for i in range(n):
+    #         for j in range(n):
+    #             if i != j:
+    #                 Mw[i,j] = nx.shortest_path_length(G,source=i,target=j,weight='weight')
+    #             else:
+    #                 Mw[i,j] = 0
+    #             #end
+    #         #end
+    #     #end
+    # #end
 
-    # Connect a random sequence of nodes in order to guarantee the existence of a Hamiltonian tour
-    permutation = list(np.random.permutation(n))
-    for (i,j) in zip(permutation,permutation[1:]+permutation[:1]):
-        Ma[i,j] = Ma[j,i] = 1
-    #end
+    # # Connect a random sequence of nodes in order to guarantee the existence of a Hamiltonian tour
+    # permutation = list(np.random.permutation(n))
+    # for (i,j) in zip(permutation,permutation[1:]+permutation[:1]):
+    #     Ma[i,j] = Ma[j,i] = 1
+    # #end
 
     # Solve
-    route = solve(Ma,Mw)
-    if route is None:
-        raise Exception('Unsolvable')
-    #end
+    vertex_cover = solve(Ma)
+    # if route is None:
+    #     raise Exception('Unsolvable')
+    # #end
 
-    return np.triu(Ma), Mw, route, nodes
+    #node means coordinate, it doesn't influence VC
+    return np.triu(Ma), vertex_cover
 #end
 
 def create_dataset(path, nmin, nmax, conn_min=1, conn_max=1, samples=1000, distances='euc_2D', metric=True):
@@ -159,10 +160,10 @@ def create_dataset(path, nmin, nmax, conn_min=1, conn_max=1, samples=1000, dista
         n = random.randint(nmin,nmax)
 
         # Create graph
-        Ma,Mw,route,nodes = create_graph(n, np.random.uniform(conn_min,conn_max), distances=distances, metric=metric)
+        Ma, vertex_cover = create_graph(n, np.random.uniform(conn_min,conn_max), distances=distances, metric=metric)
 
         # Write graph to file
-        write_graph(Ma,Mw, filepath="{}/{}.graph".format(path,i), route=route)
+        write_graph(Ma, filepath="{}/{}.graph".format(path,i), vertex_cover=vertex_cover)
 
         # Report progress
         if (i-1) % (samples//20) == 0:
@@ -173,49 +174,76 @@ def create_dataset(path, nmin, nmax, conn_min=1, conn_max=1, samples=1000, dista
     #end
 #end
 
-def write_graph(Ma, Mw, filepath, route=None, int_weights=False, bins=10**6):
+def write_graph(Ma, filepath, vertex_cover=None):
     with open(filepath,"w") as out:
 
         n, m = Ma.shape[0], len(np.nonzero(Ma)[0])
         
-        out.write('TYPE : TSP\n')
-
+        out.write('TYPE : Vertex Cover\n')
         out.write('DIMENSION: {n}\n'.format(n = n))
 
-        out.write('EDGE_DATA_FORMAT: EDGE_LIST\n')
-        out.write('EDGE_WEIGHT_TYPE: EXPLICIT\n')
-        out.write('EDGE_WEIGHT_FORMAT: FULL_MATRIX \n')
-        
-        # List edges in the (generally not complete) graph
+
         out.write('EDGE_DATA_SECTION:\n')
-        for (i,j) in zip(list(np.nonzero(Ma))[0], list(np.nonzero(Ma))[1]):
-            out.write("{} {}\n".format(i,j))
-        #end
+        for i in range(Ma.shape[0]):  
+            for j in range(i + 1, Ma.shape[1]): 
+                if Ma[i, j] == 1:
+                    out.write("{} {}\n".format(i, j))
+        # out.write('EDGE_DATA_SECTION:\n')
+        # for (i,j) in zip(list(np.nonzero(Ma))[0], list(np.nonzero(Ma))[1]):
+        #     out.write("{} {}\n".format(i,j))
+        # #end
         out.write('-1\n')
-
-        # Write edge weights as a complete matrix
-        out.write('EDGE_WEIGHT_SECTION:\n')
-        for i in range(n):
-            for j in range(n):
-                if Ma[i,j] == 1:
-                    out.write(str( int(bins*Mw[i,j]) if int_weights else Mw[i,j]))
-                else:
-                    out.write(str(n*bins+1 if int_weights else 0))
-                #end
-                out.write(' ')
-            #end
-            out.write('\n')
-        #end
-
-        if route is not None:
-            # Write route
-            out.write('TOUR_SECTION:\n')
-            out.write('{}\n'.format(' '.join([str(x) for x in route])))
-        #end
+        
+        out.write("VERTEX_COVER:\n")
+        out.write(" ".join(map(str, vertex_cover)) + "\n")
 
         out.write('EOF\n')
     #end
 #end
+
+# def write_graph(Ma, Mw, filepath, route=None, int_weights=False, bins=10**6):
+#     with open(filepath,"w") as out:
+
+#         n, m = Ma.shape[0], len(np.nonzero(Ma)[0])
+        
+#         out.write('TYPE : TSP\n')
+
+#         out.write('DIMENSION: {n}\n'.format(n = n))
+
+#         out.write('EDGE_DATA_FORMAT: EDGE_LIST\n')
+#         out.write('EDGE_WEIGHT_TYPE: EXPLICIT\n')
+#         out.write('EDGE_WEIGHT_FORMAT: FULL_MATRIX \n')
+        
+#         # List edges in the (generally not complete) graph
+#         out.write('EDGE_DATA_SECTION:\n')
+#         for (i,j) in zip(list(np.nonzero(Ma))[0], list(np.nonzero(Ma))[1]):
+#             out.write("{} {}\n".format(i,j))
+#         #end
+#         out.write('-1\n')
+
+#         # Write edge weights as a complete matrix
+#         out.write('EDGE_WEIGHT_SECTION:\n')
+#         for i in range(n):
+#             for j in range(n):
+#                 if Ma[i,j] == 1:
+#                     out.write(str( int(bins*Mw[i,j]) if int_weights else Mw[i,j]))
+#                 else:
+#                     out.write(str(n*bins+1 if int_weights else 0))
+#                 #end
+#                 out.write(' ')
+#             #end
+#             out.write('\n')
+#         #end
+
+#         if route is not None:
+#             # Write route
+#             out.write('TOUR_SECTION:\n')
+#             out.write('{}\n'.format(' '.join([str(x) for x in route])))
+#         #end
+
+#         out.write('EOF\n')
+#     #end
+# #end
 
 if __name__ == '__main__':
 
